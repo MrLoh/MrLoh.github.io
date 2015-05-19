5d06541765bc59d110feacf3c6d395a253f49199
MrLoh <flojoto@hotmail.com>
2015-05-19 15:30:22 +0200

    minor css fixes

--- assets/js/markdow.js
+++ assets/js/markdow.js
@@ -22,4 +22,4 @@ $('article blockquote').append(function() {
 }).children('a').wrap('<cite></cite>');
 
 // make links in article open in new tab
-$('article p a, article ul a, article ol a').attr('target','_blank');
+$('article a').attr('target','_blank');
